bf88c61265659c973191b3972d59c5e119e53db2
Shivam Shukla <shivamm.shukla00@gmail.com>
2026-03-30 19:33:33 +0530

    refactor(exceptions): add overloaded constructor to support optional line information in BloopException

--- src/bloop/exceptions/BloopException.java
+++ src/bloop/exceptions/BloopException.java
@@ -5,11 +5,20 @@ public abstract class BloopException extends RuntimeException {
     private final int line;
 
     protected BloopException(String message, int line) {
-        super(message);
+        super(formatMessage(message, line));
         this.line = line;
+    }
+
+    protected BloopException(String message) {
+        super(message);
+        this.line = -1; // Unknown line
     }
 
     public int getLine() {
         return line;
+    }
+
+    private static String formatMessage(String message, int line) {
+        return "[line " + line + "] " + message;
     }
 }

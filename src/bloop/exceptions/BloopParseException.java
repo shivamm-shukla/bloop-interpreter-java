2c1f7f9fe63b1266abb54daf4dbf88c389e81c72
Shivam Shukla <shivamm.shukla00@gmail.com>
2026-03-30 19:34:04 +0530

    refactor: add overloaded constructors in BloopParseException

--- src/bloop/exceptions/BloopParseException.java
+++ src/bloop/exceptions/BloopParseException.java
@@ -2,7 +2,11 @@ package bloop.exceptions;
 
 public class BloopParseException extends BloopException {
 
+    public BloopParseException(String message) {
+        super(message);
+    }
+
     public BloopParseException(String message, int line) {
-        super("Parse error at line " + line + ": " + message, line);
+        super(message, line);
     }
 }
